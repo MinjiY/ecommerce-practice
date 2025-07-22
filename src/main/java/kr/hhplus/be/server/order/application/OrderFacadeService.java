2532c1c5cce4e5bf4c42b3493b4c29a64612938f
MinjiY <upi907@naver.com>
2025-07-23 04:07:04 +0900

    [feat] 기본 패키지 구조

---
+++ src/main/java/kr/hhplus/be/server/order/application/OrderFacadeService.java
@@ -0,0 +1,7 @@
+package kr.hhplus.be.server.order.application;
+
+
+public class OrderFacadeService {
+
+
+}
---
+++ src/main/java/kr/hhplus/be/server/order/infrastructure/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package kr.hhplus.be.server.order.infrastructure.repository;
+
+
+public class OrderRepository {
+
+
+}
