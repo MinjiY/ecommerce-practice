0ae23c0c83443b8cab6ceda141f95a3b2d447c74
MinjiY <upi907@naver.com>
2025-07-25 06:19:01 +0900

    ✅test : (DomainTest) 주문 아이템의 총 금액은 주문 결제금액과 같다

--- src/test/java/kr/hhplus/be/server/order/domain/OrderTest.java
+++ src/test/java/kr/hhplus/be/server/order/domain/OrderTest.java
@@ -53,4 +53,43 @@ class OrderTest {
         });
     }
 
+    @DisplayName("주문 아이템의 총 금액이 주문의 총 금액이 같아야한다")
+    @Test
+    void orderedAmountMismatchSuccess() {
+        // given
+        Long userId = 123L;
+        Long firstProductId = 45L;
+        Long secondProductId = 46L;
+        String firstProductName = "JVM 밑바닥까지 파헤치기";
+        String secondProductName = "Effective Java";
+        Long firstProductAmount = 15000L;
+        Long secondProductAmount = 10000L;
+        Integer firstOrderQuantity = 1;
+        Integer secondOrderQuantity = 2;
+        Long expectedTotalAmount = (firstProductAmount * firstOrderQuantity) + (secondProductAmount * secondOrderQuantity);
+
+        OrderItem firstOrderItem = OrderItem.builder()
+                .userId(userId)
+                .productName(firstProductName)
+                .productAmount(firstProductAmount)
+                .orderQuantity(firstOrderQuantity)
+                .productId(firstProductId)
+                .build();
+        OrderItem secondOrderItem = OrderItem.builder()
+                .userId(userId)
+                .productName(secondProductName)
+                .productAmount(secondProductAmount)
+                .orderQuantity(secondOrderQuantity)
+                .productId(secondProductId)
+                .build();
+        List<OrderItem> orderItems = List.of(firstOrderItem, secondOrderItem);
+        Order order = Order.builder()
+                .userId(userId)
+                .totalAmount(expectedTotalAmount)
+                .build();
+
+        // when & then
+        order.orderedAmountMismatch(orderItems);
+        assertThat(order.getTotalAmount()).isEqualTo(expectedTotalAmount);
+    }
 }
