e8b734f32d662718ba1f532b0f1ade881a02c21c
MinjiY <upi907@naver.com>
2025-07-24 17:38:04 +0900

    [feat] 상품 조회 API Controller 추가

--- src/main/java/kr/hhplus/be/server/product/presentation/ProductController.java
+++ src/main/java/kr/hhplus/be/server/product/presentation/ProductController.java
@@ -1,4 +1,26 @@
 package kr.hhplus.be.server.product.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import kr.hhplus.be.server.product.application.ProductService;
+import kr.hhplus.be.server.product.application.dto.ProductServiceDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @Operation(summary = "상품 정보 조회", description = "productId로 상품 정보를 조회합니다.")
+    @GetMapping("/products/{productId}")
+    public ResponseEntity<ApiResponse<ProductServiceDTO.ProductResult>> getProduct(
+            @PathVariable Long productId
+    ) {
+        ProductServiceDTO.ProductResult productResult = productService.findProduct(productId);
+        return ResponseEntity.ok(ApiResponse.success(productResult));
+    }
 }
