0723be59911a5ff12473802f8a664c6c29bae650
MinjiY <upi907@naver.com>
2025-07-24 11:03:10 +0900

    [feat] 쿠폰 매핑테이블 추가

---
+++ src/main/java/kr/hhplus/be/server/coupon/infrastructure/entity/MapUserCouponId.java
@@ -0,0 +1,9 @@
+package kr.hhplus.be.server.coupon.infrastructure.entity;
+
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode
+public class MapUserCouponId{
+    private Long userId;
+    private Long couponId;
+}
---
+++ src/main/java/kr/hhplus/be/server/user/entity/UserEntity.java
@@ -0,0 +1,21 @@
+package kr.hhplus.be.server.user.entity;
+
+import jakarta.persistence.*;
+import kr.hhplus.be.server.coupon.common.CouponState;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "coupons")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String accounts;
+}
