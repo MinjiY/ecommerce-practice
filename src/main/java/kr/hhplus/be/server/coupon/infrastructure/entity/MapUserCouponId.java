e4ca09b413ed87d01c18d2e9497b467861f33a7d
MinjiY <upi907@naver.com>
2025-07-24 19:56:56 +0900

    [feat] 유저 쿠폰 매핑 IdClass 수정

--- src/main/java/kr/hhplus/be/server/coupon/infrastructure/entity/MapUserCouponId.java
+++ src/main/java/kr/hhplus/be/server/coupon/infrastructure/entity/MapUserCouponId.java
@@ -2,8 +2,30 @@ package kr.hhplus.be.server.coupon.infrastructure.entity;
 
 import lombok.EqualsAndHashCode;
 
-@EqualsAndHashCode
+import java.util.Objects;
+
+
 public class MapUserCouponId{
-    private Long userId;
-    private Long couponId;
+    private Long user;
+    private Long coupon;
+
+    public MapUserCouponId() {}
+
+    public MapUserCouponId(Long user, Long coupon) {
+        this.user = user;
+        this.coupon = coupon;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MapUserCouponId that = (MapUserCouponId) o;
+        return Objects.equals(user, that.user) && Objects.equals(coupon, that.coupon);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, coupon);
+    }
 }
