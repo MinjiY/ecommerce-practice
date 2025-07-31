9258837b88e6145e02f428f4391b1cac5e0397cf
MinjiY <upi907@naver.com>
2025-07-31 13:44:27 +0900

    [refactor] CouponRepository, MapUserCoupon Repository 분리 커밋 누락

--- src/main/java/kr/hhplus/be/server/coupon/infrastructure/repository/CouponRepositoryImpl.java
+++ src/main/java/kr/hhplus/be/server/coupon/infrastructure/repository/CouponRepositoryImpl.java
@@ -1,42 +1,31 @@
 package kr.hhplus.be.server.coupon.infrastructure.repository;
 
 import kr.hhplus.be.server.coupon.application.CouponRepository;
-import kr.hhplus.be.server.coupon.domain.MapUserCoupon;
-import kr.hhplus.be.server.coupon.infrastructure.entity.MapUserCouponEntity;
+import kr.hhplus.be.server.coupon.domain.Coupon;
+import kr.hhplus.be.server.coupon.infrastructure.entity.CouponEntity;
 import kr.hhplus.be.server.exception.custom.ResourceNotFoundException;
 import kr.hhplus.be.server.mapper.CouponMapper;
 import lombok.RequiredArgsConstructor;
-
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @RequiredArgsConstructor
 public class CouponRepositoryImpl implements CouponRepository {
+    private JpaRepository<CouponEntity, Long> couponJpaRepository;
 
-    private final CouponJpaRepositoryImpl mapUserCouponJpaRepository;
-
-    private final CouponMapper couponMapper;
-
-    // 사용 가능한 쿠폰 목록 조회
     @Override
-    public List<MapUserCoupon> findAvailableCouponsByUserIdAndCouponState(MapUserCoupon mapUserCoupon){
-        List<MapUserCouponEntity> mapUserCouponEntities = mapUserCouponJpaRepository.findAllByUserIdAndCouponState(mapUserCoupon.getUserId(), mapUserCoupon.getCouponState().toString());
-        return mapUserCouponEntities.stream()
-                .map(CouponMapper.INSTANCE::entityToMapUserCouponDomainWithoutUserId)
-                .toList();
+    public Coupon findByCouponId(Coupon coupon) {
+        return CouponMapper.INSTANCE.entityToCouponDomain(
+                couponJpaRepository.findById(coupon.getCouponId())
+                .orElseThrow(() -> new ResourceNotFoundException("쿠폰을 찾을 수 없습니다. id: " + coupon.getCouponId()))
+        );
     }
 
     @Override
-    public MapUserCoupon findByUserIdAndCouponId(MapUserCoupon mapUserCoupon){
-        MapUserCouponEntity mapUserCouponEntity = mapUserCouponJpaRepository.findByUserIdAndCouponId(
-                mapUserCoupon.getUserId(),
-                mapUserCoupon.getCouponId()
-        ).orElseThrow(() -> new ResourceNotFoundException("쿠폰을 찾을 수 없습니다."));
-        return CouponMapper.INSTANCE.entityToMapUserCouponDomain(mapUserCouponEntity);
+    public Coupon save(Coupon coupon){
+        CouponEntity couponEntity = CouponMapper.INSTANCE.domainToCouponEntity(coupon);
+        return CouponMapper.INSTANCE.entityToCouponDomain(couponJpaRepository.save(couponEntity));
     }
 
-    @Override
-    public MapUserCoupon save(MapUserCoupon mapUserCoupon) {
-        return couponMapper.entityToMapUserCouponDomain(mapUserCouponJpaRepository.save(couponMapper.domainToMapUserCouponEntity(mapUserCoupon)));
-    }
 
 }
+
