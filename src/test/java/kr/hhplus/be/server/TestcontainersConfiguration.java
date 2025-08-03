fc107e09a2c4b959c49694e300682bbcefcce9dc
MinjiY <upi907@naver.com>
2025-08-03 18:13:01 +0900

    [comment] 테스트컨테이너 주석제거

--- src/test/java/kr/hhplus/be/server/TestcontainersConfiguration.java
+++ src/test/java/kr/hhplus/be/server/TestcontainersConfiguration.java
@@ -1,34 +1,36 @@
-//package kr.hhplus.be.server;
-//
-//import jakarta.annotation.PreDestroy;
-//import org.springframework.boot.test.context.TestConfiguration;
-//import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.testcontainers.containers.MySQLContainer;
-//import org.testcontainers.utility.DockerImageName;
-//
-//@Configuration
-//class TestcontainersConfiguration {
-//
-//	public static final MySQLContainer<?> MYSQL_CONTAINER;
-//
-//	static {
-//		MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
-//			.withDatabaseName("hhplus")
-//			.withUsername("test")
-//			.withPassword("test");
-//		MYSQL_CONTAINER.start();
-//
-//		System.setProperty("spring.datasource.url", MYSQL_CONTAINER.getJdbcUrl() + "?characterEncoding=UTF-8&serverTimezone=UTC");
-//		System.setProperty("spring.datasource.username", MYSQL_CONTAINER.getUsername());
-//		System.setProperty("spring.datasource.password", MYSQL_CONTAINER.getPassword());
-//	}
-//
-//	@PreDestroy
-//	public void preDestroy() {
-//		if (MYSQL_CONTAINER.isRunning()) {
-//			MYSQL_CONTAINER.stop();
-//		}
-//	}
-//}
+package kr.hhplus.be.server;
+
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@Configuration
+class TestcontainersConfiguration {
+
+	public static final MySQLContainer<?> MYSQL_CONTAINER;
+
+	static {
+		MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
+			.withDatabaseName("hhplus")
+			.withUsername("test")
+			.withPassword("test")
+			.withCommand(
+					"--character-set-server=utf8mb4",
+					"--collation-server=utf8mb4_unicode_ci",
+					"--skip-character-set-client-handshake"
+			);
+		MYSQL_CONTAINER.start();
+
+		System.setProperty("spring.datasource.url", MYSQL_CONTAINER.getJdbcUrl() + "?characterEncoding=UTF-8&serverTimezone=UTC");
+		System.setProperty("spring.datasource.username", MYSQL_CONTAINER.getUsername());
+		System.setProperty("spring.datasource.password", MYSQL_CONTAINER.getPassword());
+	}
+
+	@PreDestroy
+	public void preDestroy() {
+		if (MYSQL_CONTAINER.isRunning()) {
+			MYSQL_CONTAINER.stop();
+		}
+	}
+}
